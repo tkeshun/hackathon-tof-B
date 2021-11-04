68fe5ff7ec7ec73e13031ddf57694924c54345d5
toriitorippy <toriikatsuya0707@gmail.com>
2021-11-04 19:02:01 +0900

    [5.7] 変数の埋め込み

--- tutorial/dev28/sample2/routes/index.js
+++ tutorial/dev28/sample2/routes/index.js
@@ -7,16 +7,16 @@ const router = express.Router();
  */
 
 // ルートパス「/」にアクセスした場合の処理
-router.get('/', function(request, response) {
+router.get('/', function (request, response) {
     // views/index.hbs を表示する
     response.render('index');
 });
 
 // 「/rakus」にアクセスした場合の処理
-router.get('/rakus', function(request, response) {
+router.get('/rakus', function (request, response) {
     // views/rakus.hbs を表示する
     // 第2引数で views/rakus.hbs の {{title}}, {{message}} に表示する文字をセットしている
-    response.render('rakus', {/* ここに表示したい値を記述する */});
+    response.render('rakus', { title: "こんにちは!", message: "Rakus" });
 });
 
 // ルーティングオブジェクトを外部ファイルへエクスポートする
